640e4d0616d5e6f70271ae1a4337ee7ec4b480ae
Ivan Iziumov <iiziumov@anaconda.com>
2026-01-07 10:18:31 +0100

    fix windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,24 @@
 @echo on
 
-set HAVE_READLINE=no
+copy %RECIPE_DIR%\CMakeLists.txt
 
-make prefix="%PREFIX%" -j %CPU_COUNT% install-shared
+make prefix="%LIBRARY_PREFIX%" utfdata.h
+if errorlevel 1 exit 1
+
+make prefix="%LIBRARY_PREFIX%" one.c
+if errorlevel 1 exit 1
+
+make prefix="%LIBRARY_PREFIX%" build/release/mujs.pc
+if errorlevel 1 exit 1
+
+cmake -B build -GNinja . -S %SRC_DIR% ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_BUILD_TYPE:STRING=Release
+if errorlevel 1 exit 1
+
+cmake --build build -j
+if errorlevel 1 exit 1
+
+cmake --install build
+if errorlevel 1 exit 1
